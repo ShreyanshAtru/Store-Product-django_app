6d3ad521a9593e619c9c72497160353028c62e20
ShreyanshAtru <79184646+ShreyanshAtru@users.noreply.github.com>
2022-06-16 12:11:18 +0530

    Intial commit

---
+++ product/admin.py
@@ -0,0 +1,34 @@
+from asyncio import format_helpers
+from django.contrib import admin
+from .models import Shop, Product, Category
+from django.utils.html import format_html
+
+# Register your models here.
+
+class AdminProduct(admin.ModelAdmin):
+
+    def image_tag(self,obj):
+        return format_html('<img src="{}" width="50" height="60" />'.format(obj.images.url))
+
+    list_display = ['name', 'price', 'category','image_tag', 'active']
+    search_fields = ['prod_ids','name',]
+    list_filter = ('active','price',)
+    list_display_links = ('name',)
+    list_editable = ( 'price', 'category',)
+
+    def get_queryset(self, request):
+        queryset = super(AdminProduct, self).get_queryset(request)
+        queryset = queryset.order_by('-prod_ids')
+        return queryset
+
+
+class AdminShop(admin.ModelAdmin):
+    search_fields = ['title',]
+
+class AdminCategory(admin.ModelAdmin):
+    search_fields = ['product__name',]
+
+
+admin.site.register(Shop, AdminShop)
+admin.site.register(Product, AdminProduct)
+admin.site.register(Category, AdminCategory)
---
+++ product/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+
+# Create your models here.
+
+class Category(models.Model):
+    cat_title = models.CharField(max_length=20)
+    description = models.TextField(max_length=30)  
+
+    def __str__(self):
+        return self.cat_title
+    
+    def get_all_categories():
+        return Category.objects.all()
+
+
+class Shop(models.Model):
+    title = models.CharField(max_length=20)
+    body = models.TextField()
+    image = models.ImageField(upload_to ='images', default='images/me.jpeg')
+
+    def __str__(self):
+        return self.title
+
+
+class Product(models.Model):
+    prod = models.ForeignKey(Shop, on_delete=models.CASCADE, default = 1)
+    prod_ids = models.IntegerField(primary_key=True, editable=False)
+    name = models.CharField(max_length=20)
+    price = models.FloatField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
+    description = models.TextField(max_length=100)
+    amount = models.IntegerField()
+    images = models.ImageField(upload_to ='images')
+    active = models.BooleanField()
+
+    # def get_products_by_id(prod_ids):
+    #     return Product.objects.filter(id__in =prod_ids)
+
+
+    # def get_all_products():
+    #     return Product.objects.all()
+
+    # def get_all_products_by_categoryid(category_id):
+    #     if category_id:
+    #         return Product.objects.filter(category = category_id)
+    #     else:
+    #         return Product.get_all_products()
